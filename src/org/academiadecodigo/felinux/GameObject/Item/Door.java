520f4bb2d341b656fa144beb960f5010a025b0f9
bernardocosta <bernardo.costa.90@gmail.com>
2020-06-27 13:01:28 +0100

    open/shut doors

--- src/org/academiadecodigo/felinux/GameObject/Item/Door.java
+++ src/org/academiadecodigo/felinux/GameObject/Item/Door.java
@@ -3,9 +3,20 @@ package org.academiadecodigo.felinux.GameObject.Item;
 import org.academiadecodigo.felinux.Position.MapPosition;
 import org.academiadecodigo.felinux.Support.GameObjectType;
 
-public class Door extends Item{
+public class Door extends Item {
+
+    private boolean shut;
 
     public Door(MapPosition position) {
         super(position, GameObjectType.DOOR);
+        this.shut = false;
+    }
+
+    public void shutDoor() {
+        this.shut = true;
+    }
+    
+    public void openDoor() {
+        this.shut = false;
     }
 }
