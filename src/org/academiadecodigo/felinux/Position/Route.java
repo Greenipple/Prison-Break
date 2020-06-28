cedbb615b9de77d54ea918449c5d7e61a9671304
nelsonsnatos22 <nelsonsilvasantos@outlook.com>
2020-06-28 12:08:18 +0100

    add the first part of second guard route

--- src/org/academiadecodigo/felinux/Position/Route.java
+++ src/org/academiadecodigo/felinux/Position/Route.java
@@ -6,20 +6,19 @@ import org.academiadecodigo.felinux.GameObject.Entity.Player;
 public class Route {
 
     private static int auxCounter = 13;
+    private static int move2count1 = 5;
+    private static int move2count2 = 3;
 
     public static void guard1Move(Guard guard1) throws InterruptedException {
 
 
-        System.out.println(guard1.getRectangle().getX());
-            if (guard1.getRectangle().getX() != 330 && auxCounter > 0){
-                System.out.println(guard1.getRectangle().getX());
-                Thread.sleep(200);
-                guard1.getRectangle().translate(- Map.CELL_SIZE, 0);
-                auxCounter--;
-            }
+        if (guard1.getRectangle().getX() != 330 && auxCounter > 0){
+            Thread.sleep(200);
+            guard1.getRectangle().translate(- Map.CELL_SIZE, 0);
+            auxCounter--;
+        }
 
         if (guard1.getRectangle().getX() != 850 && auxCounter == 0){
-            System.out.println(guard1.getRectangle().getX());
             Thread.sleep(200);
             guard1.getRectangle().translate(Map.CELL_SIZE, 0);
 
@@ -27,19 +26,32 @@ public class Route {
                 auxCounter = 13;
             }
         }
-            //MoveRight
-/*            while (guard1.getPosition().getCol() != (9 * Map.CELL_SIZE)){
-                Thread.sleep(500);
-                //guard1.getPosition().setPos(guard1.getPosition().getCol() + 1, guard1.getPosition().getRow());
-                guard1.getRectangle().translate(- Map.CELL_SIZE, 0);
+    }
 
-            }*/
-            //MoveLeft
-    /*        while (guard1.getPosition().getCol() != (21 * Map.CELL_SIZE)) {
-                Thread.sleep(500);
-                //guard1.getPosition().setPos(guard1.getPosition().getCol() - 1, guard1.getPosition().getRow());
-                guard1.getRectangle().translate(Map.CELL_SIZE, 0);
-            }*/
+    public static void guard2Move(Guard guard2) throws InterruptedException {
+
+       //RIGHT
+       if(guard2.getRectangle().getX() != 290 && move2count1 != 0){
+           Thread.sleep(200);
+           guard2.getRectangle().translate(Map.CELL_SIZE, 0);
+           move2count1--;
+       }
+       //DOWN
+       if(guard2.getRectangle().getY() != 530 && move2count1 == 0){
+           Thread.sleep(200);
+           guard2.getRectangle().translate(0, Map.CELL_SIZE);
+           move2count2--;
+       }
+       //LEFT
+        if(guard2.getRectangle().getX() != 290 && move2count1 == 0){
+            Thread.sleep(200);
+            guard2.getRectangle().translate(-Map.CELL_SIZE,0);
+        }
+        //DOWN
+        if(guard2.getRectangle().getY() != 610 && move2count2 == 0){
+            Thread.sleep(200);
+            guard2.getRectangle().translate(0, Map.CELL_SIZE);
+        }
     }
 }
 
