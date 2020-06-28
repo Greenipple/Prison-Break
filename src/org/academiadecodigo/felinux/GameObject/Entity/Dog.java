a39a7372a7b7301830f6ba2142f66c1b05580724
nelsonsnatos22 <nelsonsilvasantos@outlook.com>
2020-06-28 23:01:07 +0100

    dogs stay on theirs cage

--- src/org/academiadecodigo/felinux/GameObject/Entity/Dog.java
+++ src/org/academiadecodigo/felinux/GameObject/Entity/Dog.java
@@ -4,6 +4,9 @@ import org.academiadecodigo.felinux.Position.MapPosition;
 import org.academiadecodigo.felinux.Support.*;
 
 public class Dog extends Entity {
+
+    private boolean haveWall;
+
     public Dog(MapPosition position) {
         super(position, GameObjectType.DOG);
     }
@@ -13,6 +16,32 @@ public class Dog extends Entity {
 
         DirectionType direction = DirectionType.values()[(int) Math.floor(Math.random() * 4)];
 
-        this.getPosition().moveInDirection(direction, 1);
+        hitWall(direction);
+
+        if (haveWall) {
+            this.getPosition().moveInDirection(direction, 1);
+            haveWall = false;
+        }
+    }
+
+    public boolean hitWall(DirectionType directionType){
+
+        if (directionType.equals(DirectionType.UP) && this.getPosition().getRow() != 9){
+            return haveWall = true;
+        }
+
+        if (directionType.equals(DirectionType.DOWN) && this.getPosition().getRow() != 16){
+            return haveWall = true;
+        }
+
+        if (directionType.equals(DirectionType.RIGHT) && this.getPosition().getCol() != 20){
+            return haveWall = true;
+        }
+
+        if (directionType.equals(DirectionType.LEFT) && this.getPosition().getCol() != 10){
+            return haveWall = true;
+        }
+
+        return false;
     }
 }
