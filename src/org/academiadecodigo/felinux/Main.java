aeb0775caae717100b171b3c2181f35033311e66
bernardocosta <bernardo.costa.90@gmail.com>
2020-06-30 22:42:10 +0100

    put to commentary start on main

--- src/org/academiadecodigo/felinux/Main.java
+++ src/org/academiadecodigo/felinux/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
 
         Game game = new Game();
-        game.start();
+        //game.start();
         game.firstLevel();
 
     }
